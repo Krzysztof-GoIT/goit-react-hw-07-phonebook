ded1df9a211e9d8a76eabe59d4e0d1f84bab224a
Krzysztof-GoIT <145707752+Krzysztof-GoIT@users.noreply.github.com>
2024-07-29 20:48:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# goit-react-hw-06-phonebook
+
+Simple phone number notes app - created for Dev ONLY !
+
+## Screenshots
+
+![App Screenshot](https://github.com/Krzysztof-GoIT/goit-react-hw-04-phonebook/blob/main/assets/src.png?raw=true)
+
+## Usage/Examples
+
+```javascript/react
+npm run start
+```
+
+## Features
+
+- Use REDUX
+- Add/Del presistant contacts
+- Find contact by name
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,27 @@
+import css from './Filter.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFilter } from '../../redux/filterSlice';
+import { getFilter } from '../../redux/selectors';
+
+export const Filter = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(getFilter);
+
+  const handleFilterChange = e => {
+    dispatch(setFilter(e.target.value));
+  };
+
+  return (
+    <div className={css.container}>
+      <p className={css.findName}>Find Contacts by Name</p>
+      <input
+        className={css.filterInput}
+        type="text"
+        name="filter"
+        placeholder="Search by name"
+        value={filter}
+        onChange={handleFilterChange}
+      />
+    </div>
+  );
+};
